38630e51f117e4b586a77c5c0b8fb677aca3d01a
Theetouch Kasemarnontana <122422433+theetouch-k@users.noreply.github.com>
2023-03-18 15:06:08 +0700

    Fix cors error

--- api_gateway/api_gateway/settings.py
+++ api_gateway/api_gateway/settings.py
@@ -58,11 +58,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -70,6 +72,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "api_gateway.middleware.ReverseProxyMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "api_gateway.urls"
 
