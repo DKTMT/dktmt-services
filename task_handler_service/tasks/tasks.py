2daf3e5bc6524fe50811efef983a9a3cd711ad9e
Theetouch Kasemarnontana <122422433+theetouch-k@users.noreply.github.com>
2023-04-17 04:42:46 +1000

    Update duration and period time field format

--- task_handler_service/tasks/tasks.py
+++ task_handler_service/tasks/tasks.py
@@ -10,13 +10,31 @@ from .models import Ticket
 
 from task_handler_service.settings import PREDICT_SERVICE_HOST, PREDICT_SERVICE_PORT, NOTIFY_SERVICE_HOST, NOTIFY_SERVICE_PORT, TASK_HANDLER_SERVICE_HOST, TASK_HANDLER_SERVICE_PORT
 
+timeframe_lookup = {
+    "1m": 1,
+    "3m": 3,
+    "5m": 5,
+    "15m": 15,
+    "30m": 30,
+    "1h": 60,
+    "2h": 120,
+    "4h": 240,
+    "6h": 360,
+    "8h": 480,
+    "12h": 720,
+    "1d": 1440,
+    "3d": 4320,
+    "1w": 10080,
+    "1M": 43200,
+}
+
 @shared_task
 def schedule_prediction_task(ticket_id, user_data):
     ticket = Ticket.objects.get(pk=ticket_id)
 
     start_time = time.time()
-    duration = int(ticket.duration) * 60  # Assuming the duration is in minutes
-    period = int(ticket.period) * 60  # Assuming the period is in minutes
+    duration = int(timeframe_lookup.get(ticket.duration)) * 60  # Assuming the duration is in minutes
+    period = int(timeframe_lookup.get(ticket.period)) * 60  # Assuming the period is in minutes
 
     while True:
         current_time = time.time()
